88389602d2b7ee8dd65691a5de62dce7677c7ab1
A-karkaih <akarkaih@gmail.com>
2024-08-02 21:45:57 +0100

    Frontend/HomePage(pt1)

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>
+          Indulge in our diverse menu, where each dish is meticulously crafted
+          using the finest ingredients and culinary expertise. Our mission? To
+          satisfy your cravings and elevate your dining experience—one delicious
+          meal at a time
+        </p>
+        <button> View Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
